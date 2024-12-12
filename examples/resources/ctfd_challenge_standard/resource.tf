e51819ad7d818249ce6018dd90a401d113e9ccb1
Lucas TESSON <lucastesson@protonmail.com>
2024-12-12 14:45:22 +0100

    docs: sync generated docs

--- examples/resources/ctfd_challenge_standard/resource.tf
+++ examples/resources/ctfd_challenge_standard/resource.tf
@@ -1,12 +1,8 @@
-resource "ctfd_challenge" "http" {
+resource "ctfd_challenge_standard" "http" {
   name        = "My Challenge"
   category    = "misc"
   description = "..."
   value       = 500
-  decay       = 100
-  minimum     = 50
-  state       = "visible"
-  function    = "logarithmic"
 
   topics = [
     "Misc"
@@ -18,25 +14,25 @@ resource "ctfd_challenge" "http" {
 }
 
 resource "ctfd_flag" "http_flag" {
-  challenge_id = ctfd_challenge.http.id
+  challenge_id = ctfd_challenge_standard.http.id
   content      = "CTF{some_flag}"
 }
 
 resource "ctfd_hint" "http_hint_1" {
-  challenge_id = ctfd_challenge.http.id
+  challenge_id = ctfd_challenge_standard.http.id
   content      = "Some super-helpful hint"
   cost         = 50
 }
 
 resource "ctfd_hint" "http_hint_2" {
-  challenge_id = ctfd_challenge.http.id
+  challenge_id = ctfd_challenge_standard.http.id
   content      = "Even more helpful hint !"
   cost         = 50
   requirements = [ctfd_hint.http_hint_1.id]
 }
 
 resource "ctfd_file" "http_file" {
-  challenge_id = ctfd_challenge.http.id
+  challenge_id = ctfd_challenge_standard.http.id
   name         = "image.png"
   contentb64   = filebase64(".../image.png")
 }
